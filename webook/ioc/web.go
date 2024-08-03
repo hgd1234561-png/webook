d1b3c0dab2607630c231fbd391c163c6a51eda1e
jiangtaibai <2596374220@qq.com>
2024-08-03 21:42:48 +0800

    限流功能提取成pkg后做的相关代码修改

--- webook/ioc/web.go
+++ webook/ioc/web.go
@@ -2,23 +2,28 @@ package ioc
 
 import (
 	"GkWeiBook/webook/internal/web"
+	ijwt "GkWeiBook/webook/internal/web/jwt"
 	"GkWeiBook/webook/internal/web/middleware"
-	"GkWeiBook/webook/pkg/ginx/middlewares/ratelimit"
+	"GkWeiBook/webook/pkg/logger"
+	"golang.org/x/net/context"
+
+	rlimit "GkWeiBook/webook/pkg/ginx/middlewares/ratelimit"
+	"GkWeiBook/webook/pkg/ratelimit"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
 	"strings"
 	"time"
 )
 
-func InitWebServer(mdls []gin.HandlerFunc, userHdl *web.UserHandler) *gin.Engine {
+func InitWebServer(mdls []gin.HandlerFunc, userHdl *web.UserHandler, oauth2WechatHandler *web.OAuth2WechatHandler) *gin.Engine {
 	server := gin.Default()
 	server.Use(mdls...)
 	userHdl.RegisterRouters(server)
+	oauth2WechatHandler.RegisterRouters(server)
 	return server
 }
 
-func InitGinMiddlewares(redisClient redis.Cmdable) []gin.HandlerFunc {
+func InitGinMiddlewares(limiter ratelimit.Limiter, jwtHdl ijwt.Handler, log logger.Logger) []gin.HandlerFunc {
 	return []gin.HandlerFunc{
 		cors.New(cors.Config{
 			//AllowAllOrigins: true,
@@ -27,7 +32,7 @@ func InitGinMiddlewares(redisClient redis.Cmdable) []gin.HandlerFunc {
 
 			AllowHeaders: []string{"Content-Type", "Authorization"},
 			// 这个是允许前端访问你的后端响应中带的头部
-			ExposeHeaders: []string{"x-jwt-token"},
+			ExposeHeaders: []string{"x-jwt-token", "x-refresh-token"},
 			//AllowHeaders: []string{"content-type"},
 			//AllowMethods: []string{"POST"},
 			AllowOriginFunc: func(origin string) bool {
@@ -39,9 +44,15 @@ func InitGinMiddlewares(redisClient redis.Cmdable) []gin.HandlerFunc {
 			},
 			MaxAge: 12 * time.Hour,
 		}),
-		ratelimit.NewBuilder(redisClient, time.Second, 1000).Build(),
-		middleware.NewLoginJWTMiddlewareBuilder().IgnorePaths("/users/login_sms/code/send").
+		rlimit.NewBuilder(limiter).Build(),
+		middleware.NewLoginJWTMiddlewareBuilder(jwtHdl).IgnorePaths("/users/login_sms/code/send").
 			IgnorePaths("/users/login_sms").
+			IgnorePaths("/oauth2/wechat/authurl").
+			IgnorePaths("/oauth2/wechat/callback").
+			IgnorePaths("/users/refresh_token").
 			IgnorePaths("/users/signup").IgnorePaths("/users/login").Build(),
+		middleware.NewLogMiddlewareBuilder(func(ctx context.Context, l middleware.AccessLog) {
+			log.Debug("", logger.Field{Key: "req", Val: l})
+		}).AllowReqBody().AllowRespBody().Build(),
 	}
 }
